512622c35dceb75a8bcc7c7cae726a0250ca7a63
NoahELE <cxhmel@outlook.com>
2023-03-21 17:33:21 +1100

    initial commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      style = "night",
+    },
+  },
+  { "ellisonleao/gruvbox.nvim" },
+  { "nyoom-engineering/oxocarbon.nvim" },
+  { "Shatur/neovim-ayu" },
+  { "joshdick/onedark.vim" },
+  { "nordtheme/vim" },
+  { "marko-cerovac/material.nvim" },
+  { "rebelot/kanagawa.nvim" },
+  { "sainnhe/sonokai" },
+  { "sainnhe/everforest" },
+  { "sainnhe/edge" },
+}

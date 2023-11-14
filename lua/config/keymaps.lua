39143fb64355bddb9d44dd80b988c439b7b5a553
NoahELE <cxhmel@outlook.com>
2023-11-14 13:29:21 +1100

    update keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,9 +4,20 @@
 vim.keymap.set("i", "jk", "<esc>", { desc = "Esc" })
 vim.keymap.set("i", "kj", "<esc>", { desc = "Esc" })
 
-vim.keymap.set("t", "jk", "<C-\\><C-n>", { desc = "Termial to Normal Mode" })
-vim.keymap.set("t", "kj", "<C-\\><C-n>", { desc = "Termial to Normal Mode" })
-
 vim.keymap.set("n", "U", "<C-r>", { desc = "Redo" })
 
+vim.keymap.set("n", "<M-x>", ":", { desc = "Run command" })
+
 vim.keymap.set("n", "<leader>fs", "<cmd>w<cr>", { desc = "Save File" })
+
+vim.keymap.set("n", "<leader>ws", "<C-w>s", { desc = "Split window below" })
+vim.keymap.set("n", "<leader>wv", "<C-w>v", { desc = "Split window right" })
+vim.keymap.set("n", "<leader>wj", "<C-w>j", { desc = "Window below" })
+vim.keymap.set("n", "<leader>wk", "<C-w>k", { desc = "Window above" })
+vim.keymap.set("n", "<leader>wh", "<C-w>h", { desc = "Window left" })
+vim.keymap.set("n", "<leader>wl", "<C-w>l", { desc = "Window right" })
+vim.keymap.set("n", "<leader>wJ", "<C-w>J", { desc = "Move window below" })
+vim.keymap.set("n", "<leader>wK", "<C-w>K", { desc = "Move window above" })
+vim.keymap.set("n", "<leader>wH", "<C-w>H", { desc = "Move window left" })
+vim.keymap.set("n", "<leader>wL", "<C-w>L", { desc = "Move window right" })
+vim.keymap.set("n", "<leader>wo", "<C-w>L", { desc = "Enlarge current window" })

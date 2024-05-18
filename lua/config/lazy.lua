6cc61843a49834bed544b0624799f197f36a22de
NoahELE <cxhmel@outlook.com>
2024-05-18 18:44:01 +1000

    remove c++

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,8 +17,6 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    { import = "lazyvim.plugins.extras.lang.clangd" },
-    { import = "lazyvim.plugins.extras.lang.cmake" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.typescript" },

8c4500dafeb18ad3f6f61224458be70c16c55ff8
NoahELE <49080986+NoahELE@users.noreply.github.com>
2025-07-04 23:22:00 +0800

    Update plugin config

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -1,16 +1,14 @@
 return {
-  {
-    "echasnovski/mini.surround",
-    opts = {
-      mappings = {
-        add = "ys",
-        delete = "ds",
-        find = "fs",
-        find_left = "Fs",
-        highlight = "hs",
-        replace = "cs",
-        update_n_lines = "gsn",
-      },
+  "echasnovski/mini.surround",
+  opts = {
+    mappings = {
+      add = "ys",
+      delete = "ds",
+      find = "fs",
+      find_left = "Fs",
+      highlight = "hs",
+      replace = "cs",
+      update_n_lines = "gsn",
     },
   },
 }
